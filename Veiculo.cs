9cc1eba815246b1986a5697fedbf9bcd258a9c1f
brnavelar <111910980+brnavelar@users.noreply.github.com>
2023-09-25 20:40:59 -0300

    Add files via upload

---
+++ Veiculo.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIExemplo.Models
+{
+    public class Veiculo 
+    {
+        [Key]
+        [Required]
+        public int IdVeiculo { get; set; }
+        public string Descricao { get; set; }
+        public int Ano { get; set; }
+
+        public string Placa { get; set; }
+
+        public string Modelo { get; set; }
+
+        public string Marca { get; set; }
+
+        public string Cor { get; set; }
+    }
+}
